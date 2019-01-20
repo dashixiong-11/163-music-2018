8fd5f6d1221a276f6bedf11732bd20a280fa61e1
dashixiong <2788347834@qq.com>
2019-01-20 16:49:34 +0800

    添加  点击歌曲li li呈激活状态

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -9,7 +9,7 @@
       $(this.el).html(this.template)
       let {songs} = data
       let lilist = songs.map((song)=>{
-          return $('<li></li>').text(song.name)
+          return $('<li></li>').text(song.name).attr('song-id',song.id)
       })
       $(this.el).find('ul').empty()
       lilist.map((demoli)=>{
@@ -18,6 +18,9 @@
     },
     clearactive(){
       $(this.el).find('.active').remove('active')
+    },
+    activeItem(li){
+     li .addClass('active').siblings('.active').removeClass('active')
     }
   }
   let model = {
@@ -42,15 +45,31 @@
     init(view,model){
       this.view = view
       this.model = model
-      this.model.find().then(() => {
-        this.view.render(this.model.data)
-      })
+      this.bindEventHub()
+      this.getAllSongs()
+      this.bindEvents()
       this.view.render(this.model.data)
+    },
+    bindEventHub(){
       window.eventHub.on('upload',()=>{
         this.view.clearactive()
       })
       window.eventHub.on('create',(data)=>{
         this.model.data.songs.push(data)
+        this.view.render(this.model.data)
+      })
+    },
+    bindEvents(){
+      $(this.view.el).on('click','li',
+        (x)=>{
+          this.view.activeItem($(x.currentTarget))
+          let songId = x.currentTarget.getAttribute('song-id')
+          window.eventHub.emit('select',{id:songId})
+          }
+      )
+    },
+    getAllSongs(){
+      this.model.find().then(() => {
         this.view.render(this.model.data)
       })
     }
